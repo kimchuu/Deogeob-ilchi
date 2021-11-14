83fb1f139d0be3cabd710f042d97af7f3a01a428
kimminju99 <mindori1019@gmail.com>
2021-11-14 18:38:40 +0900

    Feature : Application setting(koin 생성)

---
+++ app/src/main/java/com/example/deogeobilchi/DeogeobilchiApplication.kt
@@ -0,0 +1,26 @@
+package com.example.deogeobilchi
+
+import android.app.Application
+import com.example.deogeobilchi.di.viewModelModule
+import com.example.deogeobilchi.util.PreferenceUtil
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class DeogeobilchiApplication : Application() {
+
+    companion object{
+        lateinit var prefs:PreferenceUtil
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        prefs = PreferenceUtil(applicationContext)
+        startKoin{
+            androidLogger()
+            androidContext(this@DeogeobilchiApplication)
+            modules(viewModelModule)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/deogeobilchi/di/ViewModelModule.kt
@@ -0,0 +1,7 @@
+package com.example.deogeobilchi.di
+
+import org.koin.dsl.module
+
+val viewModelModule = module {
+
+}
