5b16d41a9181c17f712b317b729bc049e8351724
kimminju99 <mindori1019@gmail.com>
2021-11-21 15:21:12 +0900

    Refactor : xml에서 onClick 구현 (annotation을 위해 kapt plugin)

--- app/src/main/java/com/example/deogeobilchi/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/deogeobilchi/ui/login/LoginActivity.kt
@@ -1,15 +1,23 @@
 package com.example.deogeobilchi.ui.login
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.databinding.BindingAdapter
 import com.example.deogeobilchi.R
 import com.example.deogeobilchi.base.BaseActivity
 import com.example.deogeobilchi.databinding.ActivityLoginBinding
+import com.example.deogeobilchi.ui.exam.ExamActivity
 
 class LoginActivity : BaseActivity() {
-    val binding by binding<ActivityLoginBinding>(R.layout.activity_login)
+    private val binding by binding<ActivityLoginBinding>(R.layout.activity_login)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.apply { }
+    }
+
+    @BindingAdapter("bind: onClick")
+    fun gotoTest(){
+        startActivity<ExamActivity>(this)
     }
 }
