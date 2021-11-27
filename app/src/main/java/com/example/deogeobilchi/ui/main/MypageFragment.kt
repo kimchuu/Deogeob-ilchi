ecb904fba91d2ec01c23d68ee6304de97ca7ea92
kimminju99 <mindori1019@gmail.com>
2021-11-27 15:44:54 +0900

    Feature : main화면 bottom navigation 구현 (Jetpack Navigation 사용)

---
+++ app/src/main/java/com/example/deogeobilchi/ui/main/MypageFragment.kt
@@ -0,0 +1,22 @@
+package com.example.deogeobilchi.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.deogeobilchi.databinding.FragmentMypageBinding
+
+class MypageFragment : Fragment() {
+    private var _binding: FragmentMypageBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMypageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
